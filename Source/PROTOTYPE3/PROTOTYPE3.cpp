6041def3af0634b841cdf48e4b5bfaadeca338d5
Peter <hguanqi7@gmail.com>
2022-04-05 16:23:01 +0800

    add AI2 animation trigger, add AI1 rotation

---
+++ Source/PROTOTYPE3/PROTOTYPE3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PROTOTYPE3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PROTOTYPE3, "PROTOTYPE3" );
